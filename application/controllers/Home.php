27e5fc721c89507b70b4f9f9e6ee4ea2c9b7ae03
HIEULE\Ngo Hieu Le <lengohieu@gmail.com>
2017-05-03 09:28:05 +0700

    Commit đang xây dựng

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,11 +4,11 @@ Class Home extends MY_Controller
 	function index(){
 		$this->load->model('slide_model');// load sile
 		$slide_list = $this->slide_model->get_list();// get du lieu slile
-
-		$data = array();		
-		$data['slide_list'] = $slide_list;
+		$data = array();
+		$data['slide_list'] = $slide_list;//data slide
 		$data['temp'] = 'site/home/index';//view index home
 		$this->load->view('site/layout', $data);
+		//$this->load->view('site/demo');// an tam thoi
 	}
 }
 ?>
--- application/views/site/home/index.php
+++ application/views/site/home/index.php
@@ -2,7 +2,7 @@
 	$this->load->view('site/slide', $this->data);
 ?>
 <div class="box-center"><!-- The box-center product-->
-             <div class="tittle-box-center">
+    <div class="tittle-box-center">
 		        <h2>Sản phẩm mới</h2>
 		      </div>
 		      <div class="box-content-center product"><!-- The box-content-center -->
