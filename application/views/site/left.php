49c50f37138c7cc9bc6960a06e73e3ae3865e1e6
HIEULE\Ngo Hieu Le <lengohieu@gmail.com>
2017-06-04 22:48:36 +0700

    Chinh CSS ti

--- application/views/site/left.php
+++ application/views/site/left.php
@@ -6,20 +6,10 @@
 	           <form class="t-form form_action" method="post" style='padding:10px' action="product/search_price.html" name="search" >
 
                   <div class="form-row">
-						<label for="param_price_from" class="form-label" style='width:70px'>Giá từ:<span class="req">*</span></label>
+						<label for="param_price_from" class="form-label" style='width:60px'>Giá từ:<span class="req">*</span></label>
 						<div class="form-item"  style='width:90px'>
 							<select  class="input" id="price_from" name="price_from" >
 							    <option value="0">0 đ</option>
-							    <option value="1000000">1,000,000 đ</option>
-							    <option value="2000000">2,000,000 đ</option>
-							    <option value="3000000">3,000,000 đ</option>
-							    <option value="4000000">4,000,000 đ</option>
-							    <option value="5000000">5,000,000 đ</option>
-							    <option value="6000000">6,000,000 đ</option>
-							    <option value="7000000">7,000,000 đ</option>
-							    <option value="8000000">8,000,000 đ</option>
-							    <option value="9000000">9,000,000 đ</option>
-							    <option value="10000000">10,000,000 đ</option>
 							</select>
 							<div class="clear"></div>
 							<div class="error" id="price_from_error"></div>
@@ -27,49 +17,10 @@
 						<div class="clear"></div>
 				  </div>
 				  <div class="form-row">
-						<label for="param_price_from" class="form-label" style='width:70px'>Giá tới:<span class="req">*</span></label>
+						<label for="param_price_from" class="form-label" style='width:60px'>Giá tới:<span class="req">*</span></label>
 						<div class="form-item"  style='width:90px'>
 							<select  class="input" id="price_to" name="price_to" >
-							     							           <option value="1000000" >
-							           1,000,000 đ</option>
-							     							           <option value="2000000" >
-							           2,000,000 đ</option>
-							     							           <option value="3000000" >
-							           3,000,000 đ</option>
-							     							           <option value="4000000" >
-							           4,000,000 đ</option>
-							     							           <option value="5000000" >
-							           5,000,000 đ</option>
-							     							           <option value="6000000" >
-							           6,000,000 đ</option>
-							     							           <option value="7000000" >
-							           7,000,000 đ</option>
-							     							           <option value="8000000" >
-							           8,000,000 đ</option>
-							     							           <option value="9000000" >
-							           9,000,000 đ</option>
-							     							           <option value="10000000" >
-							           10,000,000 đ</option>
-							     							           <option value="11000000" >
-							           11,000,000 đ</option>
-							     							           <option value="12000000" >
-							           12,000,000 đ</option>
-							     							           <option value="13000000" >
-							           13,000,000 đ</option>
-							     							           <option value="14000000" >
-							           14,000,000 đ</option>
-							     							           <option value="15000000" >
-							           15,000,000 đ</option>
-							     							           <option value="16000000" >
-							           16,000,000 đ</option>
-							     							           <option value="17000000" >
-							           17,000,000 đ</option>
-							     							           <option value="18000000" >
-							           18,000,000 đ</option>
-							     							           <option value="19000000" >
-							           19,000,000 đ</option>
-							     							           <option value="20000000" >
-							           20,000,000 đ</option>
+							  <option value="1000000" >0 đ</option>
 							     							</select>
 							<div class="clear"></div>
 							<div class="error" id="price_from_error"></div>
